9142fb8733d1def532864d564f1eec68aeb3db6e
Jake McCoy <the.mccoy@outlook.com>
2024-08-15 17:50:28 +1000

    Got the siftDown to work! Problem is it doesnt take into consideration the word. Thats tonight problem

--- Assignment1/functions.py
+++ Assignment1/functions.py
@@ -10,27 +10,43 @@ class StringPoolWithCount:
 
         self.pool.append((1, word))
 
+    def modify_tuple(self, index, new_tuple):
+        self.pool[index] = new_tuple
+
+    def access_first_element(self, index):
+        return self.pool[index]
+
+    def get_count(self, index):
+        return self.pool[index][0]
+
 
     def __str__(self):
-        print(self.pool)
+        strings = [str(item) for item in self.pool]
+        return "".join(strings)
+    
+    def __len__(self):
+        return len(self.pool)
+    
 
-def MakeHeap(array):
+def MakeHeap(array: StringPoolWithCount):
     for i in range (len(array) // 2, -1, -1):
         SiftDown(array, i)
 
-    #print(array)
 
 def SiftDown(array, index):
     parent = index
     left_child = 2 * index + 1
     right_child = 2 * index + 2
+
     if (left_child >= len(array)):
         return
     
-    if (right_child < len(array) and array[left_child] < array[right_child]):
+    if (right_child < len(array) and array.access_first_element(left_child) < array.access_first_element(right_child)):
         left_child = left_child + 1
 
-    if (array[parent] < array[left_child]):
-        array[parent], array[left_child] = array[left_child], array[parent]
-
+    
+    if (array.access_first_element(parent) < array.access_first_element(left_child)):
+        temp = array.access_first_element(parent)
+        array.modify_tuple(parent, array.access_first_element(left_child))
+        array.modify_tuple(left_child, temp)
         SiftDown(array, left_child)
--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,48 +1,52 @@
 #read in a text file
 import os
 import string
-from functions import StringPoolWithCount
+from functions import StringPoolWithCount, MakeHeap, SiftDown
 
-try:
+#try:
     #Create string pool
-    string_pool = StringPoolWithCount()
+string_pool = StringPoolWithCount()
 
-    #Get file input through a prompt
-    input_file_name = input("Hello! Please input the file you want to read from: ")
-    #print(input_file_name)
+#Get file input through a prompt
+input_file_name = input("Hello! Please input the file you want to read from: ")
+#print(input_file_name)
 
-    #Try and open it
-    file_path = os.path.join(os.path.dirname(__file__), input_file_name)
-    file = open(file_path, 'r')
+#Try and open it
+file_path = os.path.join(os.path.dirname(__file__), input_file_name)
+file = open(file_path, 'r')
 
 
-    #Array to contain all inital words
-    words = []
+#Array to contain all inital words
+words = []
 
-    #Loop through file and add elements to the stack
-    with file as f:
-        for line in f:
+#Loop through file and add elements to the stack
+with file as f:
+    for line in f:
 
-            #Rewrite to not use .strip().lower().split() and iterate through them as char array
-            line_temp = line.strip().lower().split()
-            
+        #Rewrite to not use .strip().lower().split() and iterate through them as char array
+        line_temp = line.strip().lower().split()
+        
+        temp_string = ""
+        for x in line_temp:
+            for char in x:
+                if char not in string.punctuation:
+                    temp_string = temp_string + char
+            #words.append(temp_string)
+            string_pool.addToPool(temp_string)
             temp_string = ""
-            for x in line_temp:
-                for char in x:
-                    if char not in string.punctuation:
-                        temp_string = temp_string + char
-                #words.append(temp_string)
-                string_pool.addToPool(temp_string)
-                temp_string = ""
 
 
-    print(string_pool)
+#print(string_pool)
 
-    #Close the file
-    file.close()
+MakeHeap(string_pool)
+
+print(string_pool)
+
+#Close the file
+file.close()
     
-except:
-    print("Invalid file name.\n")
+#except:
+#    print("Invalid file name.\n")
 
 
 
