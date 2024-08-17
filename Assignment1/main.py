ae54b65ac9904a6fe53f28fb7d5da86f605de9a0
TheBluesBand <the.mccoy@outlook.com>
2024-08-17 18:31:52 +1000

    input reading bug fix

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -13,7 +13,7 @@ input_file_name = input("Hello! Please input the file you want to read from: ")
 
 #Try and open it
 file_path = os.path.join(os.path.dirname(__file__), input_file_name)
-file = open(file_path, 'r')
+file = open(file_path, 'r', encoding="utf-8")
 
 #Array to contain all inital words
 words = []
