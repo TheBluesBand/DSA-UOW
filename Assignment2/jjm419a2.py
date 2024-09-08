cf94b2bdf6b66903e36019d46dbb8ef74158a3dc
TheBluesBand <the.mccoy@outlook.com>
2024-09-08 12:43:36 +1000

    Progress

--- Assignment2/jjm419a2.py
+++ Assignment2/jjm419a2.py
@@ -7,35 +7,55 @@ import os
 class Tellar:
     def __init__(self):
         self.isBusy = False
+        self.item = ""
 
     def makeBusy(self, customer):
         print(customer)
+        self.item = customer
         self.isBusy = True
 
     def checkBusy(self):
         print("nah")
         self.isBusy = False
+        self.item = ""
 
 class Queue:
     def __init__(self):
         #The basis of the queue will be a list
-        self.queue = []
+        self.p1 = []
+        self.p2 = []
+        self.p3 = []
 
     def enqueue(self, item):
         #Enqueue is adding two lists together using list comprehesion in python
-        self.queue = self.queue + [item]
+        if item[-1] == 1:
+            self.p1 = self.p1 + [item]
+        elif item[-1] == 2:
+            self.p2 = self.p2 + [item]
+        elif item[-1] == 3:
+            self.p3 = self.p3 + [item]
+        else:
+            print("Out of bounds priority " + item[-1])
 
-    def dequeue(self):
+    def dequeue(self, item):
         #Dequeue uses the list comprehension to remove the first element and save the list
-        self.queue = self.queue[1:]
+        if item[-1] == 1:
+            self.p1 = self.p1[1:]
+        elif item[-1] == 2:
+            self.p2 = self.p2[1:]
+        elif item[-1] == 3:
+            self.p3 = self.p3[1:]
+        else:
+            print("Out of bounds priority " + item[-1])
 
     def is_empty(self):
         #Check if list is empty. Return result
-        return len(self.queue) == 0
+        return len(self.p1) == 0 and len(self.p2) == 0 and len(self.p3) == 0
 
 def tellar_simulation(tellar_count):
     #Set total number of tellars
-    tellars = tellar_count
+    tellars = [Tellar] * tellar_count
+    queue = Queue
     print(tellars)
 
     #Read from the text file
@@ -44,7 +64,15 @@ def tellar_simulation(tellar_count):
 
     for line in file:
         #Allocate customer to a teller (if teller is idle)
-        print(line)
+        for tellar in tellars:
+            allocated = False
+            if tellar.isBusy() == False:
+                print(line)
+                allocated = True
+        
+        if allocated == False:
+            queue.enqueue(line)
+
 
         #If everyone is busy then add to the qyeye
 
